cedc9b46f0e9e540dcf98f5c9f9d4a66b0771bd2
Git-Fal7 <61941069+Git-Fal7@users.noreply.github.com>
2022-01-08 12:36:22 +0300

    Update grab-desktop.sh

--- grab-desktop.sh
+++ grab-desktop.sh
@@ -17,6 +17,6 @@ NAMEEXEC=`cat ${DESKTOPFILENAME} | grep "Exec=" | cut -b 6- | cut -d' ' -f 1`
 varEXEC=`echo ${NAMEEXEC} | cut -d' ' -f 1`
 cd ${ogDIRNAME}
 ./appimage-desktop /tmp/appimagedesktop/${DESKTOPFILENAME} Exec=${varEXEC} Exec=${1} > ~/.local/share/applications/${1##*/}.desktop
-
+rm -rf /tmp/appimagedesktop/
 #awk '/Exec=/{gsub(/"'"${varEXEC}"'"/, ""'"${fullfile}"'"")};{print}' ${DESKTOPFILENAME} > /tmp/onword
 #cat ${DESKTOPFILENAME} | sed "s/Exec=$\{varExec}\/Exec=$\{fullfile\}/g" > ~/.local/share/applications/${FILENAME}.desktop
